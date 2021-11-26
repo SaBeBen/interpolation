59c3415edef7bfd295020a81ca835c53f0296736
Sascha <eroifneriof@gmail.com>
2021-11-26 19:16:48 +0100

    implemented ifft

--- dft/IFFT.java
+++ dft/IFFT.java
@@ -10,12 +10,34 @@ import java.util.Arrays;
 public class IFFT {
     /**
      * Schnelle inverse Fourier-Transformation (IFFT).
-     *
+     * <p>
      * Die Funktion nimmt an, dass die Laenge des Arrays c immer eine
      * Zweierpotenz ist. Es gilt also: c.length == 2^m fuer ein beliebiges m.
      */
     public static Complex[] ifft(Complex[] c) {
-        // TODO: diese Methode ist zu implementieren
-        return null;
+        int n = c.length;
+        Complex[] v = new Complex[n];
+
+        if (n == 1) {
+            v[0] = c[0];
+        } else {
+            int m = n / 2;
+            Complex[] z1 = new Complex[n / 2];
+            for (int i = 0; i < z1.length; i++) {
+                z1[i] = c[i * 2];
+            }
+            z1 = ifft(z1);
+            Complex[] z2 = new Complex[n / 2];
+            for (int i = 0; i < z2.length; i++) {
+                z2[i] = c[i * 2 + 1];
+            }
+            z2 = ifft(z2);
+            Complex omega = Complex.fromPolar(1, 2 * Math.PI / n);
+            for (int j = 0; j < m; j++) {
+                v[j] = omega.power(j).mul(z2[j]).add(z1[j]);
+                v[m + j] = z1[j].sub(omega.power(j).mul(z2[j]));
+            }
+        }
+        return v;
     }
 }
